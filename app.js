feebce32edd3bbea664a7b90e99d89174a86c867
xbtKai <xbtKai@github.com>
2019-04-02 23:58:58 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
 //引入express模块
 const express = require('express');
 //引入用户路由器
-// const userRouter=require('./routes/user.js');
-// const productRouter=require('./routes/product.js')
 const myproRouter=require('./routes/mypro.js')
-// const homeworkRouter=require('./routes/homework.js')
 const bodyParser = require('body-parser');
 var server = express();
 server.listen(8080);
 
 
 
-// server.use(express.static('pubilc'))
-// server.use(express.static('ajaxdemo'))
-// server.use(express.static('homework'))
 server.use(express.static('mypro'))
 
 //使用body-parser中间件
@@ -21,7 +15,4 @@ server.use( bodyParser.urlencoded({
   extended:false
 }))
 //挂载路由器 挂载到/user  路由访问 /user/reg
-// server.use('/user',userRouter);
-// server.use('/product',productRouter);
 server.use('/mypro',myproRouter);
-// server.use('/homework',homeworkRouter)
